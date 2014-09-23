b6875aa5328bb8ea8dcc4d90e57dd4a870e415a4
Henry Stanley <henry@henrystanley.com>
2014-09-23 12:35:19 +0100

    Added route to index.html which has a basic text field and list

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+var projectDir = __dirname + '/'
+
 // This initialises 'app' to be a handler...
 var app = require('express')();
 // that's then supplied to an HTTP server:
 var http = require('http').Server(app);
 
-// Here, we set up the route for '/'.
-app.get('/', function(request, response){
-	response.send('<h1>Hello world!</h1>');
+// // Here, we set up the route for '/'.
+// app.get('/', function(request, response){
+// 	response.send('<h1>Hello world!</h1>');
+// });
+
+// An update on the above commented-out code:
+// we're now going to use express's sendfile method
+// instead.
+app.get('/', function(req, res){
+	res.sendFile(projectDir + 'index.html');
 });
 
 // Now, we make the HTTP server listen on port 3000.
